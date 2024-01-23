64c5f00c1dd94c30c39457f4b7b42834aac3df64
rubicon2 <liargent@hotmail.com>
2024-01-23 16:57:00 +0000

    Create a functional layout for the page
    More styling required!

---
+++ src/dom.js
@@ -0,0 +1,92 @@
+import searchIconSrc from './search.svg';
+import { publish } from './pubsub';
+
+const degreeSymbol = '\u00B0';
+
+// Store refs to the elements that will update, so we don't have to document.querySelector() every time
+let backgroundElement = null;
+let locationElement = null;
+let countryElement = null;
+let temperatureElement = null;
+let conditionElement = null;
+
+function getTemperatureStringC(temperatureC) {
+  return `${temperatureC}${degreeSymbol}C`;
+}
+
+function getTemperatureStringF(temperatureF) {
+  return `${temperatureF}${degreeSymbol}F`;
+}
+
+function createSearchBar() {
+  const searchBar = document.createElement('div');
+  searchBar.classList.add('search-bar');
+
+  const locationInput = document.createElement('input');
+  locationInput.type = 'text';
+  searchBar.appendChild(locationInput);
+
+  const searchButton = document.createElement('div');
+  searchButton.classList.add('search-button');
+
+  const searchIcon = document.createElement('img');
+  searchIcon.src = searchIconSrc;
+  searchButton.appendChild(searchIcon);
+
+  searchButton.addEventListener('click', () => {
+    if (locationInput.value) publish('onLocationSubmit', locationInput.value);
+  });
+  searchBar.appendChild(searchButton);
+
+  return searchBar;
+}
+
+function createInfoPanel() {
+  const infoPanel = document.createElement('div');
+  infoPanel.classList.add('weather-info-panel');
+
+  locationElement = document.createElement('div');
+  locationElement.classList.add('location-display');
+  infoPanel.appendChild(locationElement);
+
+  countryElement = document.createElement('div');
+  countryElement.classList.add('country-display');
+  infoPanel.appendChild(countryElement);
+
+  temperatureElement = document.createElement('div');
+  temperatureElement.classList.add('temperature-display');
+  infoPanel.appendChild(temperatureElement);
+
+  conditionElement = document.createElement('div');
+  conditionElement.classList.add('condition-display');
+  infoPanel.appendChild(conditionElement);
+
+  infoPanel.appendChild(createSearchBar());
+
+  return infoPanel;
+}
+
+async function updateWeatherDisplay(weatherObject) {
+  try {
+    locationElement.innerText = weatherObject.location;
+    countryElement.innerText = weatherObject.country;
+    temperatureElement.innerText = getTemperatureStringC(weatherObject.temp_c);
+    conditionElement.innerText = weatherObject.condition;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createWeatherDisplay() {
+  // Contains background and info display
+  const weatherDisplay = document.createElement('div');
+  weatherDisplay.classList.add('weather-container');
+  backgroundElement = weatherDisplay;
+
+  // Info panel - Takes up right side of screen, transparent background, contains text info and graphics
+  weatherDisplay.appendChild(createInfoPanel());
+
+  return weatherDisplay;
+}
+
+export { createWeatherDisplay, updateWeatherDisplay };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import { publish, subscribe } from './pubsub';
+import { createWeatherDisplay, updateWeatherDisplay } from './dom';
+import fetchWeatherData from './weatherAPI';
+
 import './style.css';
+
+document.body.appendChild(createWeatherDisplay());
+
+subscribe('onLocationSubmit', fetchWeatherData);
+subscribe('onWeatherDataFetch', updateWeatherDisplay);
+publish('onLocationSubmit', 'Sapporo');
