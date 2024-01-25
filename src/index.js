a2b95903bd4a7d5b91005eeeb3b9c14e8727bfa7
rubicon2 <liargent@hotmail.com>
2024-01-25 12:44:16 +0000

    Measure time taken for weatherAPI to respond

--- src/index.js
+++ src/index.js
@@ -5,10 +5,28 @@ import {
   updateWeatherDisplay,
   displayWeatherDataFetchError,
 } from './dom';
+import Timer from './timer';
 
 import './style.css';
 
 document.body.appendChild(createWeatherDisplay());
+
+const apiTimer = new Timer('WeatherAPI Fetch');
+subscribe('onWeatherDataFetchStart', () => {
+  apiTimer.start();
+});
+subscribe('onWeatherDataFetchSuccess', () => {
+  apiTimer.end();
+  console.log(
+    `Took ${apiTimer.elapsedTime} for weatherAPI to give us what we want!`,
+  );
+});
+subscribe('onWeatherDataFetchError', () => {
+  apiTimer.end();
+  console.log(
+    `Took ${apiTimer.elapsedTime} for weatherAPI to error out on us!`,
+  );
+});
 
 subscribe('onLocationSubmit', fetchWeatherData);
 subscribe('onWeatherDataFetchSuccess', updateWeatherDisplay);
